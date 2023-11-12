1a06b47a59d9775c4fed3508211c898981672462
Matthew Hart <mattevanhart@gmail.com>
2023-11-12 00:38:26 -0500

    don't prerender endpoint

--- src/pages/endpoint.ts
+++ src/pages/endpoint.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 export const GET: APIRoute = ({ request }) => {
   const searchParams = new URL(request.url).searchParams;
   const queryParams = [...searchParams.keys()].reduce(
